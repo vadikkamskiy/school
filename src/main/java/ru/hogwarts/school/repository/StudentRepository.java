f87f803291f86379f5e834c58bc39ec97b9f6817
vadikkamskiy <vadikkamskiy@gmail.com>
2025-04-10 19:05:16 +0300

    well done

--- src/main/java/ru/hogwarts/school/repository/StudentRepository.java
+++ src/main/java/ru/hogwarts/school/repository/StudentRepository.java
@@ -6,7 +6,5 @@ import ru.hogwarts.school.model.Student;
 import java.util.List;
 
 public interface StudentRepository extends JpaRepository<Student,Long>{
-    default List<Student> findByAge(int age) {
-        return null;
-    }
+    List<Student> findByAge(int age);
 }
