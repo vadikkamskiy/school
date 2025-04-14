96b50ba8d6e21215c24bb3c569cce722ee1b5ca4
vadikkamskiy <vadikkamskiy@gmail.com>
2025-04-14 21:26:18 +0300

    done

--- src/main/java/ru/hogwarts/school/repository/FacultyRepository.java
+++ src/main/java/ru/hogwarts/school/repository/FacultyRepository.java
@@ -6,5 +6,5 @@ import ru.hogwarts.school.model.Faculty;
 import java.util.List;
 
 public interface FacultyRepository extends JpaRepository<Faculty,Long> {
-    //List<Faculty> findByColorIgnoreCase(String color);
+    List<Faculty> findByColorIgnoreCase(String color);
 }
