b799a98aee451c6d782101626bf21fe5d192998d
vadikkamskiy <vadikkamskiy@gmail.com>
2025-05-09 14:41:22 +0300

    allTask

--- src/main/java/ru/hogwarts/school/repository/AvatarRepository.java
+++ src/main/java/ru/hogwarts/school/repository/AvatarRepository.java
@@ -4,8 +4,12 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.hogwarts.school.model.Avatar;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface AvatarRepository extends JpaRepository<Avatar, Long> {
 
     Optional<Avatar> findByStudentId(Long studentId);
     void deleteByStudentId(Long studentId);
+    Page<Avatar> findAll(Pageable pageable);
 }
