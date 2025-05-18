c84ce07433ba7031d8a36ca8c2a00d07838ad154
vadikkamskiy <vadikkamskiy@gmail.com>
2025-05-18 14:20:14 +0300

    add logs

---
+++ src/main/java/ru/hogwarts/school/controller/InfoController.java
@@ -0,0 +1,21 @@
+package ru.hogwarts.school.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/info")
+public class InfoController {
+    @Value("${server.port}")
+    private int port;
+
+    @GetMapping("")
+    public int getMethodName() {
+        return port;
+    }
+    
+}
--- src/main/java/ru/hogwarts/school/service/FacultyService.java
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -1,12 +1,14 @@
 package ru.hogwarts.school.service;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.repository.FacultyRepository;
 import java.util.*;
 
+@Slf4j
 @Service
 public class FacultyService {
     @Autowired
@@ -17,25 +19,34 @@ public class FacultyService {
     }
 
     public Faculty addFaculty(Faculty faculty) {
+        log.info("Was invoked method for create faculty");
         return facultyRepository.save(faculty);
     }
 
     public Optional<Faculty> findFaculty(long id) {
+        log.info("Was invoked method for find faculty");
         return facultyRepository.findById(id);
     }
 
     public Faculty editFaculty(Faculty faculty) {
+        log.info("Was invoked method for edit faculty");
         return facultyRepository.save(faculty);
     }
 
     public void deleteFaculty(long id) {
+        log.info("Was invoked method for delete faculty " + facultyRepository.findById(id));
+        if (facultyRepository.findById(id).isEmpty()) {
+            log.error("Faculty with id " + id + " not found");
+        }
         facultyRepository.deleteById(id);
     }
 
     public List<Faculty> findByColorIgnoreCase(String color){
+        log.info("Was invoked method for find faculty by color " + color);
         return facultyRepository.findByColorIgnoreCase(color);
     }
     public List<Faculty> getAll(){
+        log.info("Was invoked method for get all faculty");
         return facultyRepository.findAll();
     }
 }
